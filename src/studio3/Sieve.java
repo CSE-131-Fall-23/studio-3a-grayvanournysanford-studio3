c8342d93550d6f8df01fcb6fafd12357bb27d3de
Rg3abc <g.reece@wustl.edu>
2023-09-21 12:49:05 -0500

    Studio 3 is confusing :(

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,24 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter array max number: ");
+		int n = in.nextInt();
+		boolean[] primes = new boolean[n-1];
+		for (int i=2; i<=Math.sqrt(n); i++){
+			if(primes[i]==true) {
+				for(int j= i*i;j<=n;j=j+i) {
+					primes[j]=false;
+					System.out.println(primes[j]==false);
+				}
+			}
+		}
+
+	}
+
+}
